8d92a80fea303948ca7c93863faa3f11c515523f
iiaii <jhun9409@naver.com>
2020-09-02 12:59:58 +0900

    spring batch job 실행

---
+++ src/test/java/iiaii/me/springbatchinit/SpringBatchInitApplicationTests.java
@@ -0,0 +1,13 @@
+package iiaii.me.springbatchinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
